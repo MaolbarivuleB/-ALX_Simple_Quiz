b0d74b74a58045cb8496cb94280e3543dcc84349
MAOL BARIVULE BARIFAA <maalgodlove76@gmail.com>
2025-08-04 20:14:52 +0100

    Fixed quiz.js as per ALX checker requirement

--- quiz.js
+++ quiz.js
@@ -1,5 +1,4 @@
 function checkAnswer() {
-    // Retrieve the selected answer
     const selectedOption = document.querySelector('input[name="quiz"]:checked');
     const feedback = document.getElementById('feedback');
 
@@ -12,7 +11,6 @@ function checkAnswer() {
     const userAnswer = selectedOption.value;
     const correctAnswer = "4";
 
-    // Compare user's answer with the correct answer and give feedback
     if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct!";
         feedback.style.color = "green";
@@ -22,6 +20,5 @@ function checkAnswer() {
     }
 }
 
-// Retrieve the submit-answer button and add an event listener to it
 const submitButton = document.getElementById('submit-answer');
 submitButton.addEventListener('click', checkAnswer);
